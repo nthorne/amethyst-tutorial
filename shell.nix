4e907e1170d5f2dfee612912d3385802ee308931
Niklas Thörne <notrupertthorne@gmail.com>
2019-01-01 20:11:44 +0100

    Got an empty window up and running
    Added graphics dependencies, a hackish way for LD to find the needed
    libraries, and the initial code to get an empty window.

--- shell.nix
+++ shell.nix
@@ -6,14 +6,28 @@
       cmake
       expat
       freetype
+      libGL
       openssl
       pkgconfig
+      xorg.libX11
       xorg.libxcb
+      xorg.libXi
+      xorg.libXtst
+      xorg.libXcursor
+      xorg.libXrandr
 
       entr
       rustup
       rustracer
       tmuxinator
     ];
+
+    # TODO: There must be a nicer way to do this..
+    shellHook =  ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXtst}/lib";
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib";
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.xorg.libXi}/lib";
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.libGL}/lib";
+      '';
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+extern crate amethyst;
+
+use amethyst::prelude::*;
+use amethyst::renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline,
+                         RenderBundle, Stage, VirtualKeyCode};
+use amethyst::utils::application_root_dir;
+
+pub struct Pong;
+
+/**
+ * SimpleState handles a lot of the basics, such as handing updates and events,
+ * which we would have to implement ourselves otherwise.
+ */
+impl SimpleState for Pong {
+
+}
+
+// Returns a amethyst::Result so that we can use `.?` for exit on setup failure.
+fn main() -> amethyst::Result<()> {
+
+    // Start the amethyst logger, with a default config so that we get messages
+    // and warnings
+    amethyst::start_logger(Default::default());
+
+    // Load the display configuration from the Rust Object Notation file
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+    let config = DisplayConfig::load(&path);
+
+    // Render a black background
+    let pipe = Pipeline::build()
+        .with_stage(
+            Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+            );
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderBundle::new(pipe, Some(config))
+            .with_sprite_sheet_processor()
+        )?;
+
+    // Bind the OS event loop and the amethyst components
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    // Start the game loop
+    game.run();
+    Ok(())
 }
